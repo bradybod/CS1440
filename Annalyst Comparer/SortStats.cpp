0bd381c9d2691854605f2cf31cfc53b89f6890e4
bradybodily <bradybod@gmail.com>
2017-02-13 17:15:10 -0700

    remove bad directory

--- Annalyst Comparer/SortStats.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "SortStats.hpp"
-
-std::string printSortStats(SortStats& rStats)
-{
-	std::stringstream ss;
-	ss << rStats.compareCount << " compares, " << rStats.swapCount << " swap count, " << rStats.time << " seconds";
-	return ss.str();
-}
